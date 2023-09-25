d3e873f30dd7037a2fab808a1ba7af69b86ad51f
mhmamouch <98241045+MohamedHmamouch@users.noreply.github.com>
2023-09-26 01:05:39 +0200

    longest common substring

--- Data_structure_algorithms.py
+++ Data_structure_algorithms.py
@@ -428,6 +428,32 @@ def highestValuePalindrome(s, n,k):
 
 
 
+def LCS(s1,s2):
+
+    n,m=len(s1),len(s2)
+
+    dp=[[0]*(m+1) for _ in range(n+1)]
+
+
+    for i, c1 in enumerate(s1):
+
+        for j , c2 in enumerate(s2):
+
+            if c1==c2:
+
+                dp[i][j]=1+ dp[i-1][j-1]
+
+            else:
+
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+
+    return dp[n-1][m-1]
+
+
+
+
+
+
 
 
                 
