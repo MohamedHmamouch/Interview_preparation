c48db9410d1f3e2010cb08e42d5c01a44d8f5fc2
mhmamouch <98241045+MohamedHmamouch@users.noreply.github.com>
2024-02-15 20:57:24 +0100

    update colorful_field

--- B_sheet_code_forces/colorful_field.py
+++ B_sheet_code_forces/colorful_field.py
@@ -4,15 +4,57 @@
 
 if __name__=='__main__':
 
-    n,m,k,t=map(int,input().split())
+    n, m, k, t = map(int, input().split())
+    ans = 0
+    wasted = []
+    for _ in range(k):
+        a, b = map(int, input().split())
+        wasted.append((a, b))
 
-    matrix=[]
+    wasted.sort()
 
-    for _ in range(k+t):
-        a,b=map(int,input().split())
-        matrix.append([a,b])
+    for _ in range(t):
+        i, j = map(int, input().split())
+        if (i, j) in wasted:
+            print('Waste')
+            continue
+        else:
+            g = (wasted + [(i, j)])
+            g.sort()
+            x = g.index((i, j))
+            ans = (m*(i-1) + j) - x
+            print('Carrots' if ans%3 == 1 else 'Kiwis' if ans%3 == 2 else 'Grapes')
+    
+    # n, m, k, t = map(int, input().split())
+    # waste_pos = []
+    # plants = ["Carrots", "Kiwis", "Grapes"]
 
-    matrix.sort(key=lambda x:x[0])
+    # for _ in range(k):
+    #     a, b = map(int, input().split())
+    #     a -= 1
+    #     b -= 1
+    #     waste_pos.append(m * a + b)
 
-    print(matrix)
+    # for _ in range(t):
+    #     i, j = map(int, input().split())
+    #     i -= 1
+    #     j -= 1
+    #     pos = m * i + j
+
+    #     waste_before_cell = 0
+    #     is_waste = False
+    #     for p in waste_pos:
+    #         if p == pos:
+    #             is_waste = True
+    #             break
+    #         elif p < pos:
+    #             waste_before_cell += 1
+
+    #     if is_waste:
+    #         print("Waste")
+    #     else:
+    #         print(plants[(pos - waste_before_cell) % 3])
+
+
+    
 
