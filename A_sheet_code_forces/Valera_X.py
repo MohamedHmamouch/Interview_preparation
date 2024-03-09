e7d4dc73a7df6ac32b0e288e42a7b76ac1b34f63
mhmamouch <98241045+MohamedHmamouch@users.noreply.github.com>
2024-03-10 00:57:13 +0100

    new solution to valera and X

--- A_sheet_code_forces/Valera_X.py
+++ A_sheet_code_forces/Valera_X.py
@@ -39,6 +39,33 @@ if __name__=='__main__':
         print("NO")
 
 
+    # mat=[]
+
+    # for _ in range(n):
+
+    #     mat.append(list(input()))
+
+    # first_diag=mat[0][0]
+    # second_diag=mat[0][n-1]
+    # ref=mat[0][1]
+    # unique=set()
+    # for i in range(len(mat)):
+
+    #     for j in range(len(mat)):
+
+    #         if mat[i][i]!=first_diag or mat[i][n-1-i]!=second_diag:
+
+    #             print("NO")
+    #             exit()
+
+    #         elif i!=j and j!=n-1-i:
+    #               if mat[i][j]!=ref or mat[i][j]==first_diag or mat[i][j]==second_diag:
+
+    #                 print("NO")
+    #                 exit()
+    # print("YES")
+
+
 
 
 
