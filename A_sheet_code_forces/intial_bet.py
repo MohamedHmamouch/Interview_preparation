62fd788164d0d700d89c72afc6265a1e85f46092
mhmamouch <98241045+MohamedHmamouch@users.noreply.github.com>
2024-03-16 16:51:58 +0100

    Create intial_bet.py

---
+++ A_sheet_code_forces/intial_bet.py
@@ -0,0 +1,14 @@
+
+
+if __name__=='__main__':
+
+    l=list(map(int,input().split()))
+
+    total_sum=sum(l)
+    if total_sum==0:
+        print(0)
+    elif total_sum%5!=0 or total_sum==0:
+        print("-1")
+
+    else:
+        print(total_sum/5)
