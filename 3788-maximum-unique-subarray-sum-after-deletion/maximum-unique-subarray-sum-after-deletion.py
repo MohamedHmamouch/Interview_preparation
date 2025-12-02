92899e646b040f2ae333017849b17264b4d14bdc
Mohamed HMAMOUCH <98241045+MohamedHmamouch@users.noreply.github.com>
2025-12-02 21:34:48 +0100

    Time: 4 ms (6.65%) | Memory: 17.8 MB (56.07%) - LeetSync

--- 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
+++ 3788-maximum-unique-subarray-sum-after-deletion/maximum-unique-subarray-sum-after-deletion.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
+        
 
         seen=set()
 
-        current_sum=0
-
-        max_total=float('-inf')
+        total=0
+        max_total=0
 
         for k in nums:
 
@@ -14,9 +14,10 @@ class Solution:
                 seen.add(k)
 
                 if k>=0:
-                    current_sum+=k
+                    
+                    total+=k
 
-            
-            max_total=max(max_total,current_sum)
+            max_total=max(max_total,total)
 
+        
         return max_total if max_total>0 else max(nums)
