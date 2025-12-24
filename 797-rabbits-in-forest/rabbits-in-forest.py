307338e06192f603ec9c1ab28482de3c0fecc887
Mohamed HMAMOUCH <98241045+MohamedHmamouch@users.noreply.github.com>
2025-12-24 20:09:51 +0100

    Time: 4 ms (8.41%) | Memory: 17.7 MB (96.15%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,23 +1,23 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
+        
 
-
+        total=0
         freq={}
+        import math
 
         for ans in answers:
 
             freq[ans]=1+freq.get(ans,0)
 
-        total=0
+        print(freq)
+
         for k,v in freq.items():
 
+            size_groups=k+1
 
-            number_rabbits_in_group=k+1
+            num_groups=math.ceil(v/size_groups)
 
-            groups_to_construct=math.ceil(v/number_rabbits_in_group)
-
-            total+=number_rabbits_in_group*groups_to_construct
+            total+=size_groups*num_groups
 
         return total
-
-        
