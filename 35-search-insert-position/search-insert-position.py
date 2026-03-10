7ae538923e7104f3bc747ce0380ddee90d4c2454
Mohamed HMAMOUCH <98241045+MohamedHmamouch@users.noreply.github.com>
2026-03-10 01:46:27 +0100

    Time: 0 ms (100.00%) | Memory: 20 MB (78.68%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -8,16 +8,18 @@ class Solution:
 
             mid=(l+r)//2
 
+
             if nums[mid]==target:
 
                 return mid
 
-            elif target>nums[mid]:
 
-                l=mid+1
+            elif nums[mid]>target:
+
+                r=mid-1
 
             else:
 
-                r=mid-1
+                l=mid+1
 
         return l
