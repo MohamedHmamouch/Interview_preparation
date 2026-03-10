5e148a8f5bd91b8fd134d4996a71bcd84c847d06
Mohamed HMAMOUCH <98241045+MohamedHmamouch@users.noreply.github.com>
2026-03-10 01:08:28 +0100

    Time: 0 ms (100.00%) | Memory: 19.4 MB (56.89%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,31 +1,31 @@
-
-import math
 class Solution:
     def mySqrt(self, x: int) -> int:
+        
 
+        closest=0
 
-        l=1
-        r=x
+        l,r=0,x
 
-        nearest=x
 
         while l<=r:
 
+
             mid=(l+r)//2
 
-            if mid*mid==x:
+
+            if mid**2==x:
 
                 return mid
 
-            elif mid*mid>x:
+
+            elif mid**2>x:
 
                 r=mid-1
 
             else:
 
-                nearest=mid
+                closest=max(closest,mid)
+
                 l=mid+1
 
-        return nearest
-
-        
+        return closest
