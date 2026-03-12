93bcc8225043d86f772eb725f7f387655a6af698
Mohamed HMAMOUCH <98241045+MohamedHmamouch@users.noreply.github.com>
2026-03-12 15:27:47 +0100

    Time: 218 ms (15.39%) | Memory: 25.7 MB (87.46%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,32 +1,30 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         
+
+        freq={}
+        unique_fruits=set(fruits)
+
         l,r=0,0
 
-        max_picked=0
-
-        basket={}
         total=0
 
         while r<len(fruits):
 
-            basket[fruits[r]]=1+basket.get(fruits[r],0)
-            total+=1
+            freq[fruits[r]]=1+freq.get(fruits[r],0)
 
-            while l<=r and len(basket)>2:
 
-                total-=1
-                basket[fruits[l]]-=1
+            while l<r and len(freq)>2:
 
-                if basket[fruits[l]]<=0:
+                freq[fruits[l]]-=1
 
-                    del basket[fruits[l]]
+                if freq[fruits[l]]<=0:
+
+                    del freq[fruits[l]]
 
                 l+=1
 
+            total=max(total,r-l+1)
             r+=1
-            max_picked=max(max_picked,total)
 
-        return max_picked
-
-
+        return total
