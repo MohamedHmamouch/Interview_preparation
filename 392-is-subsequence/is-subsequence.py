46270661eaf23020d9d5cf217ccb69eee3c77605
Mohamed HMAMOUCH <98241045+MohamedHmamouch@users.noreply.github.com>
2026-05-29 23:01:45 +0200

    Time: 0 ms (100.00%) | Memory: 19.4 MB (7.56%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,29 +1,26 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-
-        res=""
+        
 
         p1=0
+
         p2=0
 
-        num_char_s=sum([1 for char in s])
-        num_char_t=sum([1 for char in t])
+        counter=0
 
-        print(num_char_s,num_char_t)
-
-        while p1<num_char_s and p2<num_char_t:
+        while p1<len(s) and p2<len(t):
 
             if s[p1]==t[p2]:
 
-                res+=s[p1]
                 p1+=1
-                p2+=1
-
-            else:
 
                 p2+=1
 
-        print(s)
-        print(res)
-        return s==res
+                counter+=1
 
+            elif s[p1]!=t[p2]:
+
+                p2+=1
+
+        
+        return counter==len(s)
