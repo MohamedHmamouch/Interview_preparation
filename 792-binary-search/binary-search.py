7b3562120817a0b8aebfbcf68e930c6a8af6b9ed
Mohamed HMAMOUCH <98241045+MohamedHmamouch@users.noreply.github.com>
2026-05-30 21:36:32 +0200

    Time: 0 ms (100.00%) | Memory: 20.5 MB (75.13%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,5 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        
 
         left=0
         right=len(nums)-1
@@ -8,17 +9,17 @@ class Solution:
 
             mid=(left+right)//2
 
-            if nums[mid]>target:
-
-                right=mid-1
-
-            elif nums[mid]<target:
-
-                left=mid+1
-
-            elif nums[mid]==target:
+            if nums[mid]==target:
 
                 return mid
 
+            
+            elif nums[mid]>target:
+
+                right=mid-1
+
+            else:
+
+                left=mid+1
+
         return -1
-        
